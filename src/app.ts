0e7e9f65b3e6497d24f1a54b1fe50eb1db49a7e8
mercadoalex <mercadoalex@gmail.com>
2025-02-23 11:22:25 -0600

    adding lokk and feel

--- src/app.ts
+++ src/app.ts
@@ -41,6 +41,15 @@ app.post('/generate-badge', async (req: Request, res: Response): Promise<void> =
             <html>
                 <head>
                     <title>Badge Generation Error</title>
+                    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/animate.css">
+                    <link rel="stylesheet" type="text/css" href="/css/hamburgers.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/animsition.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/select2.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/daterangepicker.css">
+                    <link rel="stylesheet" type="text/css" href="/css/util.css">
+                    <link rel="stylesheet" type="text/css" href="/css/main.css">
                 </head>
                 <body>
                     <h1>Badge Generation Error</h1>
@@ -58,6 +67,15 @@ app.post('/generate-badge', async (req: Request, res: Response): Promise<void> =
             <html>
                 <head>
                     <title>Badge Generation Error</title>
+                    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/animate.css">
+                    <link rel="stylesheet" type="text/css" href="/css/hamburgers.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/animsition.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/select2.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/daterangepicker.css">
+                    <link rel="stylesheet" type="text/css" href="/css/util.css">
+                    <link rel="stylesheet" type="text/css" href="/css/main.css">
                 </head>
                 <body>
                     <h1>Badge Generation Error</h1>
@@ -75,6 +93,15 @@ app.post('/generate-badge', async (req: Request, res: Response): Promise<void> =
             <html>
                 <head>
                     <title>Badge Generation Error</title>
+                    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/animate.css">
+                    <link rel="stylesheet" type="text/css" href="/css/hamburgers.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/animsition.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/select2.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/daterangepicker.css">
+                    <link rel="stylesheet" type="text/css" href="/css/util.css">
+                    <link rel="stylesheet" type="text/css" href="/css/main.css">
                 </head>
                 <body>
                     <h1>Badge Generation Error</h1>
@@ -105,6 +132,15 @@ app.post('/generate-badge', async (req: Request, res: Response): Promise<void> =
             <html>
                 <head>
                     <title>Badge Generated</title>
+                    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/animate.css">
+                    <link rel="stylesheet" type="text/css" href="/css/hamburgers.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/animsition.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/select2.min.css">
+                    <link rel="stylesheet" type="text/css" href="/css/daterangepicker.css">
+                    <link rel="stylesheet" type="text/css" href="/css/util.css">
+                    <link rel="stylesheet" type="text/css" href="/css/main.css">
                 </head>
                 <body>
                     <h1>Badge Generated Successfully</h1>
@@ -114,7 +150,11 @@ app.post('/generate-badge', async (req: Request, res: Response): Promise<void> =
                         <li><a href="http://${SERVER_IP}:${PORT}/share-badge">Share Badge on LinkedIn</a></li>
                     </ul>
                     <p><a href="/">Go back</a></p>
+
                 </body>
+
+
+              
             </html>
         `); // Sending the URL of the generated badge as the response
     } catch (error) {
@@ -131,10 +171,34 @@ app.get('/', (req: Request, res: Response) => {
     const keyCodeOptions = Object.keys(keyCodeCatalog).map(keyCode => `<option value="${keyCode}">${keyCode}</option>`).join('');
 
     res.send(`
-        <html>
+       <!DOCTYPE html>
+        <html lang="en">
             <head>
-                <title>Welcome</title>
-                <script>
+                <title>Welcome To the Digital Badge Creator</title>
+                	<meta charset="UTF-8">
+	                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <!--===============================================================================================-->
+	            <link rel="icon" type="image/png" href="images/icons/favicon.ico">
+                <!--===============================================================================================-->
+	            <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+                <!--===============================================================================================-->
+	            <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css">
+                <!--===============================================================================================-->
+	            <link rel="stylesheet" type="text/css" href="/css/animate.css">
+                <!--===============================================================================================-->
+	            <link rel="stylesheet" type="text/css" href="/css/hamburgers.min.css">
+                <!--===============================================================================================-->
+	            <link rel="stylesheet" type="text/css" href="/css/animsition.min.css">
+                <!--===============================================================================================-->
+	            <link rel="stylesheet" type="text/css" href="/css/select2.min.css">
+                <!--===============================================================================================-->
+	            <link rel="stylesheet" type="text/css" href="/css/daterangepicker.css">
+                <!--===============================================================================================-->
+	            <link rel="stylesheet" type="text/css" href="/css/util.css">
+	            <link rel="stylesheet" type="text/css" href="/css/main.css">
+                <!--===============================================================================================-->
+                <meta name="robots" content="noindex, follow">
+                    <script>
                     const keyCodeCatalog = ${JSON.stringify(keyCodeCatalog)};
                     function confirmSubmission(event) {
                         const keyCode = document.getElementById('keyCode').value;
@@ -148,6 +212,7 @@ app.get('/', (req: Request, res: Response) => {
             </head>
             <body>
                 <h1>Welcome to the Badge Generation Service</h1>
+                <h3>Recognize accomplishments with 100% verifiable digital badges</h3>
                 <form id="badgeForm" action="/generate-badge" method="post" onsubmit="confirmSubmission(event)">
                     <label for="firstName">First Name:</label>
                     <input type="text" id="firstName" name="firstName" required><br>
@@ -164,6 +229,101 @@ app.get('/', (req: Request, res: Response) => {
                     <input type="hidden" id="hiddenField" name="hiddenField" value="${hiddenFieldValue}"><br>
                     <button type="submit">Generate Badge</button>
                 </form>
+
+                dfsgfhfkhjklfjhjflghklfkhfglhkñgfhkgflhkfghlñfgkhlgfñhklñghklfghkgfñlhkfghklfgkhkfglhkfglks
+
+	<div class="container-contact100">
+		<div class="wrap-contact100">
+			<form class="contact100-form validate-form">
+				<span class="contact100-form-title">
+					Say Hello!
+				</span>
+
+				<div class="wrap-input100 validate-input" data-validate="Name is required">
+					<span class="label-input100">Your Name</span>
+					<input class="input100" type="text" name="name" placeholder="Enter your name">
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<span class="label-input100">Email</span>
+					<input class="input100" type="text" name="email" placeholder="Enter your email addess">
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 input100-select">
+					<span class="label-input100">Needed Services</span>
+					<div>
+						<select class="selection-2" name="service">
+							<option>Choose Services</option>
+							<option>Online Store</option>
+							<option>eCommerce Bussiness</option>
+							<option>UI/UX Design</option>
+							<option>Online Services</option>
+						</select>
+					</div>
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 input100-select">
+					<span class="label-input100">Budget</span>
+					<div>
+						<select class="selection-2" name="budget">
+							<option>Select Budget</option>
+							<option>1500 $</option>
+							<option>2000 $</option>
+							<option>2500 $</option>
+						</select>
+					</div>
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="wrap-input100 validate-input" data-validate = "Message is required">
+					<span class="label-input100">Message</span>
+					<textarea class="input100" name="message" placeholder="Your message here..."></textarea>
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="container-contact100-form-btn">
+					<div class="wrap-contact100-form-btn">
+						<div class="contact100-form-bgbtn"></div>
+						<button class="contact100-form-btn">
+							<span>
+								Submit
+								<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+							</span>
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+	<div id="dropDownSelect1"></div>
+
+    <!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+	<script>
+		$(".selection-2").select2({
+			minimumResultsForSearch: 20,
+			dropdownParent: $('#dropDownSelect1')
+		});
+	</script>
+    <!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
             </body>
         </html>
     `);
